296cc781338078e92cc3de1100749976d04a18be
togana <o.togana@gmail.com>
2017-03-18 13:13:46 +0900

    add route NotFound

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
 import { ConnectedRouter } from 'react-router-redux';
 
 import App from './containers/App';
+import NotFound from './containers/NotFound';
 import configureStore from './store/configureStore';
 
 const history = createHistory();
@@ -14,7 +15,10 @@ const store = configureStore(history);
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <Route exact path="/" component={App} />
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route component={NotFound} />
+      </Switch>
     </ConnectedRouter>
   </Provider>,
   document.getElementById('root'),
